9d768a4e0aa77829b3ade6493735cf1a43b8995a
COMLABPC13-PC\ALLAN_Novalta <allannovalta@gmail.com>
2018-08-18 16:48:40 +0800

    Models

---
+++ AllanNovalta.CSMViewGrade.Web/Infrastructure/Data/Helper/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AllanNovalta.CSMViewGrade.Web.Infrastructure.Data.Helper
+{
+    public class BaseModel
+    {
+        public Guid? Id { get; set; }
+        public DateTime Timestamp { get; set; }
+        public BaseModel()
+        {
+            this.Timestamp = DateTime.UtcNow;
+        }
+    }
+}
